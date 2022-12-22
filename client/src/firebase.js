7fbbe06056aa23f459f8b55019167254efb5b430
challelign <chalie2123@gmail.com>
2022-12-22 19:49:52 +0300

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from "firebase";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqYfbKpe2vbKhk7mhOcnKOm_Z1kUiqK6M",
+  authDomain: "complex-96972.firebaseapp.com",
+  projectId: "complex-96972",
+  storageBucket: "complex-96972.appspot.com",
+  messagingSenderId: "356144245905",
+  appId: "1:356144245905:web:1aa08b69b31312858de330",
+  measurementId: "G-S6952S65ML"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// export
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
---
+++ client/src/pages/auth/Register.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+
+const Register = () => {
+
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = () => {
+
+    }
+
+    const registerForm = () => (
+        <form onSubmit={handleSubmit}>
+            <input type="email" className="form-control"
+                   id="email"
+                   value={email}
+                   onChange={(e) => setEmail(e.target.value)}
+                   autoFocus
+            />
+            <button type="submit" className="btn btn-raised pt-2">Register /{email}</button>
+        </form>
+    );
+
+    return (
+
+        <div className="container p-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <h4>Register</h4>
+                    {registerForm()}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
