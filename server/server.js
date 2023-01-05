2d2b3084e212f7bc29a2245318ab05c0200b5973
Challelign <chalie2123@gmail.com>
2023-01-05 15:03:37 +0300

    no more change

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
@@ -10,25 +9,29 @@ require("dotenv").config();
 const app = express();
 
 // db
-mongoose
-  .connect(process.env.DATABASE, {
+mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
-  })
-  .then(() => console.log("DB CONNECTED"))
-  .catch((err) => console.log("DB CONNECTION ERR", err));
+})
+    .then(() => console.log("DB CONNECTED"))
+    .catch((err) => console.log("DB CONNECTION ERR", err));
 
 // middlewares
+
+// It is a great tool that logs the requests along with some other information depending upon its configuration and the preset used.
 app.use(morgan("dev"));
-app.use(bodyParser.json({ limit: "2mb" }));
+
+// bodyParser->This middleware is used to parse the body of an HTTP request, which can contain JSON data or other types of data
+// available under the req.body property
+app.use(bodyParser.json({limit: "2mb"}));
 app.use(cors());
 
 // route
 app.get("/api", (req, res) => {
-  res.json({
-    data: "hey you hit node API",
-  });
+    res.json({
+        data: "hey you hit node API",
+    });
 });
 
 // port
